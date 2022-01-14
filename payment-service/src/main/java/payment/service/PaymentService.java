cbcd76fd5c0ef0e8afedb591da2c06504a99050f
BINGKUN WU <elias@BINGKUNs-MBP.localdomain>
2022-01-14 17:49:52 +0100

    TokenValidated and PaymentCompleted event defined

--- payment-service/src/main/java/payment/service/PaymentService.java
+++ payment-service/src/main/java/payment/service/PaymentService.java
@@ -1,22 +1,55 @@
 package payment.service;
 
+import domain.Payment;
+import dtu.ws.fastmoney.BankService;
 import messaging.Event;
 import messaging.MessageQueue;
+import transaction.service.BankTransactionService;
+
+import java.util.HashMap;
 
 public class PaymentService {
     MessageQueue queue;
+    HashMap<String, Payment> pendingPayments; //<customerToken, payment>
+    BankTransactionService bankTransactionService;
 
     public PaymentService(MessageQueue q) {
         this.queue = q;
-        this.queue.addHandler("TokenRequested", this::handleTokenRequested);
+        this.queue.addHandler("PaymentInitiated", this::handlePaymentInitiated);
+        this.queue.addHandler("TokenValidated", this::handleTokenValidated);
+        pendingPayments = new HashMap<>();
+        bankTransactionService = new BankTransactionService();
     }
 
-    public void handleTokenRequested(Event ev) {
-        var customerId = ev.getArgument(0, String.class);
-//        System.out.println(customerId);
-//        var token = new Payment();
-//        token.setToken("4321");
-//        Event event = new Event("TokenProvided", new Object[] { token });
-//        queue.publish(event);
+    public void handlePaymentInitiated(Event e) {
+        // publish a "TokenValidationRequested" event
+        Payment payment = e.getArgument(0, Payment.class);
+        String customerToken = payment.getCustomerToken();
+        if (pendingPayments.containsKey(customerToken)) { // token is in the pending payments
+               // return error, close payment
+            return; //todo
+        }
+        else {
+            pendingPayments.put(customerToken, payment);
+        }
+        Event event = new Event("TokenValidationRequested", new Object[] {customerToken});
+        queue.publish(event);
+
+    }
+
+    public void handleTokenValidated(Event e) {
+        String customerToken = e.getArgument(0, String.class);
+        // find out the corresponding pending payment
+        Payment payment = pendingPayments.get(customerToken);
+        // conduct the bank transaction
+
+        // remove the payment from pending
+        pendingPayments.remove(customerToken);
+        // record the completed payment
+
+        // publish "PaymentCompleted" event
+        String description = "";
+        Event event = new Event("PaymentCompleted", new Object[] {description});
+        queue.publish(event);
     }
 }
