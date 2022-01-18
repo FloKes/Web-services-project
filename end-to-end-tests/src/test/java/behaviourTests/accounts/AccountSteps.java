980ffbf58d64a0fd077bb1aec68aa9ec1ab83351
s202771 <s202771@student.dtu.dk>
2022-01-18 10:54:20 +0100

    Race tests and new .bat file

--- end-to-end-tests/src/test/java/behaviourTests/accounts/AccountSteps.java
+++ end-to-end-tests/src/test/java/behaviourTests/accounts/AccountSteps.java
@@ -20,29 +20,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountSteps {
     DtuApiService service = new DtuApiService();
-    private AccountDTO account;
-    private CompletableFuture<AccountDTO> result = new CompletableFuture<>();
-    private AccountDTO accountReceived;
+    private AccountDTO account1;
+    private CompletableFuture<AccountDTO> result1 = new CompletableFuture<>();
+    private AccountDTO accountReceived1;
+    private AccountDTO account2;
+    private CompletableFuture<AccountDTO> result2 = new CompletableFuture<>();
+    private AccountDTO accountReceived2;
 
     @Given("person with name {string} {string} with cpr {string}, bank accountId {string}")
     public void personWithNameWithCprBankAccountId(String firstName, String lastName, String cpr, String bankAccountId) {
-        account = new AccountDTO();
-        account.setFirstname(firstName);
-        account.setLastname(lastName);
-        account.setCpr(cpr);
-        account.setBankAccount(bankAccountId);
-        assertNull(account.getAccountId());
+        account1 = new AccountDTO();
+        account1.setFirstname(firstName);
+        account1.setLastname(lastName);
+        account1.setCpr(cpr);
+        account1.setBankAccount(bankAccountId);
+        assertNull(account1.getAccountId());
     }
+
     @When("the user is being registered")
     public void theUserIsBeingRegistered() {
-        result.complete(service.requestAccount(account));
+        result1.complete(service.requestAccount(account1));
     }
     @Then("the user is registered")
     public void theUserIsRegistered() {
-        accountReceived = result.join();
+        accountReceived1 = result1.join();
     }
     @Then("has a non empty id")
     public void hasANonEmptyId() {
-        assertNotNull(accountReceived.getAccountId());
+        assertNotNull(accountReceived1.getAccountId());
+    }
+
+    @Given("second person with name {string} {string} with cpr {string}, bank accountId {string}")
+    public void secondPersonWithNameWithCprBankAccountId(String firstName, String lastName, String cpr, String bankAccountId) {
+        account2 = new AccountDTO();
+        account2.setFirstname(firstName);
+        account2.setLastname(lastName);
+        account2.setCpr(cpr);
+        account2.setBankAccount(bankAccountId);
+        assertNull(account2.getAccountId());
+    }
+
+    @When("the two accounts are registered at the same time")
+    public void theTwoAccountsAreRegisteredAtTheSameTime() {
+        var thread1 = new Thread(() -> {
+            result1.complete(service.requestAccount(account1));
+        });
+        var thread2 = new Thread(() -> {
+            result2.complete(service.requestAccount(account2));
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+    @Then("the first account has a non empty id")
+    public void theFirstAccountHasANonEmptyId() {
+        var accountDTO = result1.join();
+        assertEquals(account1.getFirstname(), accountDTO.getFirstname());
+        assertNotNull(accountDTO.getAccountId());
+    }
+
+    @Then("the second account has a non empty id different from the first student")
+    public void theSecondAccountHasANonEmptyIdDifferentFromTheFirstStudent() {
+        var accountDTO1 = result1.join();
+        var accountDTO2 = result2.join();
+        assertEquals(account2.getFirstname(), accountDTO2.getFirstname());
+        assertNotNull(accountDTO2.getAccountId());
+        System.out.println("Accoutn 1 id: " + accountDTO1.getAccountId());
+        System.out.println("Accoutn 2 id: " + accountDTO2.getAccountId());
+        assertNotEquals(accountDTO1.getAccountId(), accountDTO2.getAccountId());
     }
 }
--- end-to-end-tests/src/test/java/behaviourTests/features/accounts.feature
+++ end-to-end-tests/src/test/java/behaviourTests/features/accounts.feature
@@ -4,3 +4,10 @@ Feature: AccountRequestFeature
     When the user is being registered
     Then the user is registered
     And has a non empty id
+
+  Scenario: Account Registration Race Condition
+    Given person with name "Florian" "Kesten" with cpr "000000-1234", bank accountId "56741"
+    And second person with name "Bingkun" "Wu" with cpr "000000-5678", bank accountId "45897"
+    When the two accounts are registered at the same time
+    Then the first account has a non empty id
+    And the second account has a non empty id different from the first student
